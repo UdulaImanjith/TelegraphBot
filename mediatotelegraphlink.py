994f547b80792a839f515af82ed2e899ef1eb688
Udula_Official <87321778+UdulaImanjith@users.noreply.github.com>
2022-05-30 17:34:12 +0530

    Initial commit

--- mediatotelegraphlink.py
+++ mediatotelegraphlink.py
@@ -34,7 +34,7 @@ async def start(client, message):
 @teletips.on_message(filters.media & filters.private)
 async def get_link_private(client, message):
     try:
-        text = await message.reply("Processing...")
+        text = await message.reply("ᴘʀᴏᴄᴇꜱꜱɪɴɢ...")
         async def progress(current, total):
             await text.edit_text(f"📥 ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴍᴇᴅɪᴀ... {current * 100 / total:.1f}%")
         try:
@@ -42,10 +42,10 @@ async def get_link_private(client, message):
             local_path = await message.download(location, progress=progress)
             await text.edit_text("📤 ᴜᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ...")
             upload_path = upload_file(local_path) 
-            await text.edit_text(f"**🌐 | ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ**:\n\n<code>https://telegra.ph{upload_path[0]}</code>\n\n©️ ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ")     
+            await text.edit_text(f"**🌐 | ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ**:\n\n<code>https://telegra.ph{upload_path[0]}</code>\n\n©️ [ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ](https://t.me/isotrop)")     
             os.remove(local_path) 
         except Exception as e:
-            await text.edit_text(f"**❌ | ꜰɪʟᴇ ᴜᴘʟᴏᴀᴅ ꜰᴀɪʟᴇᴅ**\n\n<i>**Reason**: {e}</i>\n\n©️ ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ")
+            await text.edit_text(f"**❌ | ꜰɪʟᴇ ᴜᴘʟᴏᴀᴅ ꜰᴀɪʟᴇᴅ**\n\n<i>**Reason**: {e}</i>\n\n©️ [ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ](https://t.me/isotrop)")
             os.remove(local_path) 
             return                 
     except Exception:
@@ -54,7 +54,7 @@ async def get_link_private(client, message):
 @teletips.on_message(filters.command('tl'))
 async def get_link_group(client, message):
     try:
-        text = await message.reply("Processing...")
+        text = await message.reply("ᴘʀᴏᴄᴇꜱꜱɪɴɢ...")
         async def progress(current, total):
             await text.edit_text(f"📥 ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴍᴇᴅɪᴀ... {current * 100 / total:.1f}%")
         try:
@@ -62,10 +62,10 @@ async def get_link_group(client, message):
             local_path = await message.reply_to_message.download(location, progress=progress)
             await text.edit_text("📤 ᴜᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ...")
             upload_path = upload_file(local_path) 
-            await text.edit_text(f"**🌐 | ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ**:\n\n<code>https://telegra.ph{upload_path[0]}</code>\n\n©️ ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ")     
+            await text.edit_text(f"**🌐 | ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ**:\n\n<code>https://telegra.ph{upload_path[0]}</code>\n\n©️ [ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ](https://t.me/isotrop)")     
             os.remove(local_path) 
         except Exception as e:
-            await text.edit_text(f"**❌ | ꜰɪʟᴇ ᴜᴘʟᴏᴀᴅ ꜰᴀɪʟᴇᴅ**\n\n<i>**Reason**: {e}</i>\n\n©️ ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ")
+            await text.edit_text(f"**❌ | ꜰɪʟᴇ ᴜᴘʟᴏᴀᴅ ꜰᴀɪʟᴇᴅ**\n\n<i>**Reason**: {e}</i>\n\n©️ [ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ](https://t.me/isotrop)")
             os.remove(local_path) 
             return         
     except Exception:
