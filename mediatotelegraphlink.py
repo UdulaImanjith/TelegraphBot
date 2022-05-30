63c23010ea83543d4c86d23cac775764efebde88
Udula_Official <87321778+UdulaImanjith@users.noreply.github.com>
2022-05-30 16:26:37 +0530

    Update mediatotelegraphlink.py

--- mediatotelegraphlink.py
+++ mediatotelegraphlink.py
@@ -21,13 +21,12 @@ async def start(client, message):
 ɪ ᴄᴀɴ ᴇᴀꜱɪʟʏ ɢᴇɴᴇʀᴀᴛᴇ ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋꜱ ꜰᴏʀ ʏᴏᴜʀ ᴍᴇᴅɪᴀ ꜰɪʟᴇꜱ 📥
 
 ʏᴏᴜ ᴊᴜꜱᴛ ʜᴀᴠᴇ ᴛᴏ ꜱᴇɴᴅ ᴀ ᴠᴀʟɪᴅ ᴍᴇᴅɪᴀ ꜰɪʟᴇ ᴀɴᴅ ɪ ᴡɪʟʟ ᴅᴏ ᴛʜᴇ ʀᴇꜱᴛ 😏
-➡ (ᴠᴀʟɪᴅ ꜰɪʟᴇ ᴛʏᴘᴇꜱ ᴀʀᴇ 'jpeg', 'jpg', 'png', 'mp4' and 'gif')
+➡ (ᴠᴀʟɪᴅ ꜰɪʟᴇ ᴛʏᴘᴇꜱ ᴀʀᴇ ᴊᴘᴇɢ', 'ᴊᴘɢ', 'ᴘɴɢ', 'ᴍᴘ4' ᴀɴᴅ 'ɢɪꜰ')
 
 💠 ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ʟɪɴᴋꜱ ɪɴ **ɢʀᴏᴜᴘ ᴄʜᴀᴛꜱ**, ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ꜱᴜᴘᴇʀɢʀᴏᴜᴘ ᴀɴᴅ ꜱᴇɴᴅ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ <code>/tl</code> ᴀꜱ ᴀ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴠᴀʟɪᴅ ᴍᴇᴅɪᴀ ꜰɪʟᴇ.
 
-👤 | Developed By [Nexus 💫](https://t.me/Nexus_8)
-❇️ | Project By [Isotrop Community](https://t.me/isotrop)
-🤖 | [Official Bot](https://t.me/Isotrop_Bot)
+👤 | ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ [ɴᴇxᴜꜱ 💫](https://t.me/Nexus_8)
+💫 | ᴘʀᴏᴊᴇᴄᴛ ʙʏ [ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ](https://t.me/isotrop)
             """
     await teletips.send_message(message.chat.id, text, disable_web_page_preview=True)
     
@@ -37,16 +36,16 @@ async def get_link_private(client, message):
     try:
         text = await message.reply("Processing...")
         async def progress(current, total):
-            await text.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
+            await text.edit_text(f"📥 ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴍᴇᴅɪᴀ... {current * 100 / total:.1f}%")
         try:
             location = f"./media/private/"
             local_path = await message.download(location, progress=progress)
-            await text.edit_text("📤 Uploading to Telegraph...")
+            await text.edit_text("📤 ᴜᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ...")
             upload_path = upload_file(local_path) 
-            await text.edit_text(f"**🌐 | Telegraph Link**:\n\n<code>https://telegra.ph{upload_path[0]}</code>")     
+            await text.edit_text(f"**🌐 | ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ**:\n\n<code>https://telegra.ph{upload_path[0]}</code>\n\n©️ [ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ](https://t.me/isotrop)")     
             os.remove(local_path) 
         except Exception as e:
-            await text.edit_text(f"**❌ | File upload failed**\n\n<i>**Reason**: {e}</i>")
+            await text.edit_text(f"**❌ | ꜰɪʟᴇ ᴜᴘʟᴏᴀᴅ ꜰᴀɪʟᴇᴅ**\n\n<i>**Reason**: {e}</i>\n\n©️ [ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ](https://t.me/isotrop)")
             os.remove(local_path) 
             return                 
     except Exception:
@@ -57,16 +56,16 @@ async def get_link_group(client, message):
     try:
         text = await message.reply("Processing...")
         async def progress(current, total):
-            await text.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
+            await text.edit_text(f"📥 ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴍᴇᴅɪᴀ... {current * 100 / total:.1f}%")
         try:
             location = f"./media/group/"
             local_path = await message.reply_to_message.download(location, progress=progress)
-            await text.edit_text("📤 Uploading to Telegraph...")
+            await text.edit_text("📤 ᴜᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ...")
             upload_path = upload_file(local_path) 
-            await text.edit_text(f"**🌐 | Telegraph Link**:\n\n<code>https://telegra.ph{upload_path[0]}</code>")     
+            await text.edit_text(f"**🌐 | ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ**:\n\n<code>https://telegra.ph{upload_path[0]}</code>\n\n©️ [ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ](https://t.me/isotrop)")     
             os.remove(local_path) 
         except Exception as e:
-            await text.edit_text(f"**❌ | File upload failed**\n\n<i>**Reason**: {e}</i>")
+            await text.edit_text(f"**❌ | ꜰɪʟᴇ ᴜᴘʟᴏᴀᴅ ꜰᴀɪʟᴇᴅ**\n\n<i>**Reason**: {e}</i>\n\n©️ [ɪꜱᴏᴛʀᴏᴘ ᴄᴏᴍᴍᴜɴɪᴛʏ](https://t.me/isotrop)")
             os.remove(local_path) 
             return         
     except Exception:
